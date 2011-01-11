da6bcc1a0f3aa4faebdc6290dab210ec84483cc2
Q <q@qwitter-client.net>
2011-01-10 20:00:28 -0500

    Cleaned up updater code

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ def die():
 
 from updater import AutoUpdater
 A = AutoUpdater("http://dl.dropbox.com/u/4410208/bn.zip", "test.zip", "bootstrapper.sh", finish_callback=die)
+A.start_update()
 
 
--- updater.py
+++ updater.py
@@ -10,19 +10,19 @@ import stat
 
 class AutoUpdater(object):
 
- def __init__(self, URL, location, bootstrapper, MD5=None, percentage_callback=None, finish_callback=None):
+ def __init__(self, URL, save_location, bootstrapper, MD5=None, percentage_callback=None, finish_callback=None):
   """Supply a URL/location/bootstrapper filename to download a zip file from
      The finish_callback argument should be a Python function it'll call when done"""
   #Let's download the file using urllib
   self.complete = 0
   self.finish_callback = finish_callback #What to do on exit
   self.percentage_callback = percentage_callback or self.print_percentage_callback
+  self.URL = URL
+  self.bootstrapper = bootstrapper
+  self.MD5 = MD5
   #Change our location to not be local -- use platform_utils
-  location = os.path.join(platform_utils.paths.app_data_path('updater'), location)
+  self.save_location = os.path.join(platform_utils.paths.app_data_path('updater'), save_location)
   platform_utils.paths.prepare_app_data_path('updater')
-  #os.chmod(platform_utils.paths.app_data_path('updater'), 777)
-  #os.chmod(platform_utils.paths.app_data_path('updater'), stat.S_IRWXU)
-  self.start(URL, location, bootstrapper, MD5)
 
 
  def hookProg(self, count, bSize, tSize):
@@ -34,38 +34,37 @@ class AutoUpdater(object):
  def print_percentage_callback(percent):
   print percent
 
- def start(self, URL, location, bootstrapper, MD5):
-  """Called by __init__ to start the whole process"""
-  Listy = urllib.urlretrieve(URL, location, reporthook=self.hookProg)
-  if MD5:
+ def start_update(self):
+  """Called to start the whole process"""
+  Listy = urllib.urlretrieve(self.URL, self.save_location, reporthook=self.hookProg)
+  if self.MD5:
    #Check the MD5
-   if self.MD5File(location) != MD5:
+   if self.MD5File(location) != self.MD5:
     #ReDownload
     print "MD5 HASH FAIL -- RETRY DOWNLOAD"
-    self.start(URL, location, bootstrapper, MD5)
+    self.start_update()
    else:
     #It redownloaded properly
-    self.DoneDownload(Listy[0], bootstrapper)
+    self.download_complete(Listy[0])
   else:
    #We don't know if it downloaded properly
    #The programmer didn't give us an MD5. So we have to assume it was valid.
-   self.DoneDownload(Listy[0], bootstrapper)
+   self.download_complete(Listy[0])
 
  def MD5File(self, fileName):
   "Custom function that will get the Md5 sum of our file"
   file_reference=open(fileName, 'rb').read() 
   return hashlib.md5(file_reference).hexdigest()
 
- def DoneDownload(self, location, bootstrapper):
+ def download_complete(self, location):
   """Called when the file is done downloading, and MD5 has been successfull"""
   CurD = os.getcwd() #Store our current working directory (of main app)
   print location
  # os.chmod(location, 777)
   zippy = ZipFile(location, 'r')
   Pathy = os.path.join(platform_utils.paths.app_data_path('updater'), os.path.basename(location).strip(".zip"))
-  #os.chmod(Pathy, 775)
   zippy.extractall(Pathy)
-  BootStr = os.path.join(Pathy, bootstrapper)
+  BootStr = os.path.join(Pathy, self.bootstrapper)
   os.chmod(BootStr, stat.S_IRUSR|stat.S_IXUSR)
   print BootStr
   subprocess.call([BootStr + " -l " + Pathy], shell=True)
