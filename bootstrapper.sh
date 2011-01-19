a466f24b8ca7ef03da3a00d01bdbb05547559670
ryan <ryan@localhost>
2011-01-18 21:17:55 -0500

    Now uses cp instead of mv, and deletes when done. Works perfecly on Linux..to my knowledge.

--- bootstrapper.sh
+++ bootstrapper.sh
@@ -3,8 +3,15 @@
 #bootstrapper.sh
 #This script was originally written in Python, but converted to BASH/AutoIt for size
 
+# Absolute path to this script. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f $0)
+# Absolute path this script is in. /home/user/bin
+SCRIPTPATH=`dirname $SCRIPT`
+
 POSPAR1="$1" #-l
 POSPAR2="$2" #location
 POSPAR3="$3" #-d
 POSPAR4="$4" #directory
-mv $4/* $2
+echo "$SCRIPTPATH/$4/*" $2
+cp -r -f $SCRIPTPATH/$4/* $2
+rm -r -f $SCRIPTPATH/$4
