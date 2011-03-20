46d8c7e1cf9fa30424917ebd33fddc232ffae898
ryan <ryan@localhost>
2011-03-20 00:06:30 -0400

    Fixed shell script on mac.

--- bootstrapper.sh
+++ bootstrapper.sh
@@ -1,19 +1,14 @@
-#!/bin/bash
-
+#!/bin/bash  
 #bootstrapper.sh
-#This script was originally written in Python, but converted to BASH/AutoIt for size
-
 PIDD="$5"
-kill -0 $pid >/dev/null
+while sleep 1; do kill -0 $PIDD || break; done
 # Absolute path to this script. /home/user/bin/foo.sh
-SCRIPT=$(readlink -f $0)
+SCRIPT=$(cd ${0%/*} && echo $PWD/${0##*/})
 # Absolute path this script is in. /home/user/bin
 SCRIPTPATH=`dirname $SCRIPT`
-
 POSPAR1="$1" #-l
 POSPAR2="$2" #location
 POSPAR3="$3" #-d
 POSPAR4="$4" #directory
 cp -r -f $SCRIPTPATH/$4/* $2
 rm -r -f $SCRIPTPATH/$4
-exit 1
