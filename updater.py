a799859669c7eec6b6014823c3c9a88257945c18
Q <q@qwitter-client.net>
2011-07-17 19:52:39 -0400

    Wrote a Version object for better version number comparisons in the updater

--- updater.py
+++ updater.py
@@ -6,6 +6,7 @@ logger = logging.getLogger('updater')
 
 from urllib import FancyURLopener, URLopener
 import urllib2
+from functools import total_ordering
 import hashlib
 import os
 try:
@@ -124,14 +125,89 @@ class AutoUpdater(object):
    return
 
 def find_update_url(URL, version):
-  """Return a URL to an update of the application for the current platform at the given URL if one exists, or None""
-     Assumes Windows, Linux, or Mac"""
-  response = urllib2.urlopen(URL)
-  json_str = response.read().strip("\n")
-  json_p = json.loads(json_str)
-  if json_p['current_version'] > version:
-    return json_p['downloads'][platform.system()]
+ """Return a URL to an update of the application for the current platform at the given URL if one exists, or None""
+  Assumes Windows, Linux, or Mac"""
+ response = urllib2.urlopen(URL)
+ json_str = response.read().strip("\n")
+ json_p = json.loads(json_str)
+ if is_newer(version, json_p['current_version']):
+  return json_p['downloads'][platform.system()]
 
+def is_newer(local_version, remote_version):
+  """Returns True if the remote version is newer than the local version."""
+  return Version(remote_version) > local_version
+
+
+
+@total_ordering
+class Version(object):
+ VERSION_QUALIFIERS = {
+  'alpha': 1,
+  'beta': 2,
+  'rc': 3
+ }
+
+ def __init__(self, version):
+  self.version = version
+  self.version_qualifier = None
+  self.version_qualifier_num = None
+  self.sub_version = None
+  if isinstance(version, basestring):
+   version = version.lower()
+   if '-' not in version:
+    for q in self.VERSION_QUALIFIERS:
+     if q in version:
+      self.version_qualifier = q
+      self.version_qualifier_num = self.VERSION_QUALIFIERS[q]
+      split_version = version.split(q)
+      self.version_number = float(split_version[0])
+      if len(split_version) > 1:
+       self.sub_version = split_version[1]
+      return
+    self.version_number= float(version)
+    return
+   split_version = version.split('-')
+   self.version_number= float(split_version[0])
+   self.version_qualifier = split_version [1]
+   self.version_qualifier_num = self.VERSION_QUALIFIERS[self.version_qualifier]
+   if len(split_version) == 3:
+    self.sub_version = int(split_version[2])
+  else:
+   self.version_number= float(version)
+
+ def __lt__(self, other):
+  if not isinstance(other, self.__class__):
+   other = Version(other)
+  if other.version_qualifier == self.version_qualifier == None:
+   return self.version_number< other.version_number
+  if self.version_number < other.version_number:
+   return True
+  elif self.version_number > other.version_number:
+   return False
+  if other.version_number == self.version_number and not other.version_qualifier_num and self.version_qualifier_num:
+   return True
+  if other.version_number == self.version_number and self.version_qualifier_num == self.version_qualifier_num and self.sub_version < other.sub_version:
+   return True
+  return self.version_qualifier_num < other.version_qualifier_num
+
+ def __gt__(self, other):
+  if not isinstance(other, self.__class__):
+   other = Version(other)
+  if other.version_qualifier == self.version_qualifier == None:
+   return self.version_number > other.version_number
+  if self.version_number < other.version_number:
+   return False
+  elif self.version_number > other.version_number:
+   return True
+  if other.version_number == self.version_number and not other.version_qualifier_num and self.version_qualifier_num:
+   return False
+  if other.version_number == self.version_number and self.version_qualifier_num == self.version_qualifier_num and self.sub_version > other.sub_version:
+   return True
+  return self.version_qualifier_num > other.version_qualifier_num
+
+
+
+  
 class CustomURLOpener(FancyURLopener):
  def http_error_default(*a, **k):
   return URLopener.http_error_default(*a, **k)
